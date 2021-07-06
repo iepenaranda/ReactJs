0276f9a528e8c8637f6be41c42085f9bd5d396d4
iepenaranda <iepo960709@gmail.com>
2021-07-06 12:00:39 -0500

    first commit

---
+++ PatronObserver.js
@@ -0,0 +1,54 @@
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(notifyingClass) {
+    this.observers.push(notifyingClass);
+  }
+
+  unsubscribe(notifyingClass) {
+    this.observers = this.observers.filter(
+      (observer) => observer instanceof notifyingClass !== true
+    );
+  }
+
+  notifyObservable(mensaje) {
+    this.observers.forEach((observer) => {
+      observer.notify(mensaje);
+    });
+  }
+}
+
+class NumberExample extends Observable {
+  constructor() {
+    super();
+    this.value = 0;
+  }
+
+  increment() {
+    this.value++;
+    this.notifyObservable(this);
+  }
+}
+
+class NumberExampleSpanish {
+  notify(mensaje) {
+    console.log(`El nuevo número es: ${mensaje.value}`);
+  }
+}
+
+class NumberExampleEnglish {
+  notify(mensaje) {
+    console.log(`The new number is: ${mensaje.value}`);
+  }
+}
+
+let numberExample = new NumberExample();
+let nee = new NumberExampleEnglish();
+
+numberExample.subscribe(new NumberExampleSpanish());
+numberExample.subscribe(nee);
+
+numberExample.increment();
+numberExample.increment();
